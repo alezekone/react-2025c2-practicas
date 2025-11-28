6a6718ab2d9c3558ae4364cd66b9afbba3cfe218
Alejandro E. Aguilar <alezek@gmail.com>
2025-11-28 06:17:21 -0300

    Armado del estadío intermedio del proyecto antes de Context API. Parte 2

---
+++ Clase_08_P00/src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <nav style={{ backgroundColor: "#333", color: "white", padding: "10px" }}>
+      <ul style={{ listStyle: "none", display: "flex", justifyContent: "space-around", margin: 0 }}>
+        {/*En react-router-dom, usamos el COMPONENTE <Link> en lugar del ELEMENTO HTML <a>*/}
+        {/*
+        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>Home</a></li>
+        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>About</a></li>
+        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>Contact</a></li>
+        */}
+        <li><Link to={'/'}>Inicio</Link></li>
+        <li><Link to={'/admin'}>Admin</Link></li>
+        <li><Link to={'/contacto'}>Contacto</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
